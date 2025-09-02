bfc6c399346d28ba25ca48a844bd0d5e27aeae87
MelArt24 <animator2404@gmail.com>
2025-09-02 18:10:04 +0300

    Update Detail Activity UI

---
+++ app/src/main/java/com/am24/onlineshopapp/activity/DetailActivity.kt
@@ -0,0 +1,15 @@
+package com.am24.onlineshopapp.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.am24.onlineshopapp.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
